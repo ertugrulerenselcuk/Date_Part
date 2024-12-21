75e0699698e9939030452529d22d3039fcf540d8
Ertugrul Eren Selcuk <83607933+ertugrulerenselcuk@users.noreply.github.com>
2024-12-21 12:43:49 +0300

    dateFunction.sql

---
+++ dateFunction.sql
@@ -0,0 +1,12 @@
+SELECT CURRENT_DATE,
+	DATE_PART('century', CURRENT_DATE) as century_,
+	DATE_PART('quarter', CURRENT_DATE) as quarter,
+	DATE_PART('decade', CURRENT_DATE) as decade,
+	DATE_PART('year', CURRENT_DATE) as year_,
+	DATE_PART('month', CURRENT_DATE) as month_,
+	DATE_PART('day', CURRENT_DATE) as day_,
+	DATE_PART('hour', CURRENT_DATE) as hour_,
+	DATE_PART('minute', CURRENT_DATE) as minute_,
+	DATE_PART('dow', CURRENT_DATE) as dow_,
+	DATE_PART('doy', CURRENT_DATE) AS doy_,
+	DATE_PART('timezone', CURRENT_TIME) as timezone_;
